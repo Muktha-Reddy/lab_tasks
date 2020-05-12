e028aa5452c623ec0761d0b23a95e965807b8327
Muktha-Reddy <mk706906@dal.ca>
2020-05-12 13:36:13 -0300

    task1 is done

--- lab4/task1.py
+++ lab4/task1.py
@@ -1,14 +1,16 @@
-s=open("alice.txt")
+import string
+s=open("alice.txt",'r')
 inp = s.readlines()
 #print(inp)
-def wrd(l):
-  #k=[]
-  for i in l:
-    i.strip("\n")
-    for j in i:
-      j.strip("")
-      
-    print(j)
-    #print(i)
-
-wrd(inp)
+def wrd():
+  for i in inp:
+     nl = i.split(' ')
+     #print(nl)
+     for j in nl:
+       b=j.strip("\n")
+       for k in b:
+         if k in string.punctuation:
+           b = b.replace(k,"")
+       b=b.lower()
+       print(b)
+wrd()
